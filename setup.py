6d95006e25ee9545f04d80e3bee40acff996d675
yu-ki3406 <yuukidesuichiharadesu@gmail.com>
2023-02-09 22:13:06 +0900

    Update : setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Syumi-note',
+    version='0.1.0',
+    description='Learning something',
+    author='Yuki Ichihara',
+    author_email='y.ichihara3406@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'numpy==1.24.2',
+        'scipy==1.10.0',
+        'matplotlib==3.6.3',
+        'jax==0.4.2'
+    ],
+)
